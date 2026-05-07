c40a56094e125c7a30715a008fd7d1b703137462
NguyenThiNhii <nhi7158456@gmail.com>
2026-05-07 22:35:37 +0700

    change language

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,16 +1,22 @@
 <?php
 
 return [
+    'settings_title' => 'Settings',
+    'settings_subtitle' => 'Manage settings and account options.',
+    'display_and_language' => 'Display & Language',
     'dark_mode' => 'Dark Mode',
-    'dark_mode_desc' => 'Reduce eye strain in low light environments',
+    'dark_mode_desc' => 'Reduce eye strain in low light environments.',
     'language' => 'Language',
     'language_desc' => 'Change the display language of the interface',
+    'lang_vi' => 'Vietnamese',
+    'lang_en' => 'English',
     
+    'storage' => 'Storage',
     'storage_usage' => 'Current cache storage usage',
     'clear_cache' => 'Clear Cache',
 
     'device_management' => 'Device Management',
-    'devices_active' => 'active devices',
+    'devices_active' => 'Active Devices',
     'current' => 'CURRENT',
     'browser_chrome' => 'Chrome Browser',
     'minutes_ago' => 'minutes ago',
@@ -20,6 +26,7 @@ return [
     'privacy' => 'Privacy Policy',
     'support' => 'Customer Support',
     'about' => 'About NHOMJ',
+
     'danger_zone_title' => 'Danger Zone',
     'danger_zone_desc' => 'Permanently delete or disable your account.',
     'disable_account' => 'Disable Account',
