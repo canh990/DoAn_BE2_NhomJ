85b9e75d37ba60c6e7950aa106e3778bb36afba7
NguyenThiNhii <nhi7158456@gmail.com>
2026-05-30 11:43:24 +0700

    commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,17 +8,30 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $feedType = $request->input('feed', 'recommend');
+        if (!in_array($feedType, ['recommend', 'following'])) {
+            $feedType = 'recommend';
+        }
+
         $blockedUserIds = [];
-        if (auth()->check()) {
-            $user = auth()->user();
+        $followingIds = [];
+        $user = auth()->user();
+
+        if ($user) {
             $blockedByMe = $user->blockedUsers()->pluck('nguoi_bi_chan_id')->toArray();
             $blockedMe = $user->blockedByUsers()->pluck('nguoi_chan_id')->toArray();
             $blockedUserIds = array_unique(array_merge($blockedByMe, $blockedMe));
+
+            $followingIds = \DB::table('theo_doi')
+                ->where('nguoi_theo_doi_id', $user->id)
+                ->where('trang_thai', 'da_chap_nhan')
+                ->pluck('nguoi_duoc_theo_doi_id')
+                ->toArray();
         }
 
-        $posts = BaiViet::with(['user', 'media', 'originalPost.user', 'originalPost.media', 'poll.options.votes', 'poll.votes'])
+        $query = BaiViet::with(['user', 'media', 'originalPost.user', 'originalPost.media', 'poll.options.votes', 'poll.votes'])
             ->withCount(['reactions', 'comments', 'shares'])
             ->with(['reactions' => function ($query) {
                 $query->where('nguoi_dung_id', auth()->id());
@@ -28,13 +41,34 @@ class HomeController extends Controller
                 $query->where('nguoi_dung_id', auth()->id());
             }])
             ->where('da_xoa', false)
-            ->whereIn('loai', ['van_ban', 'hinh_anh', 'chia_se', 'binh_chon'])
-            ->when(!empty($blockedUserIds), function ($query) use ($blockedUserIds) {
-                $query->whereNotIn('nguoi_dung_id', $blockedUserIds);
-            })
-            ->latest()
-            ->take(20)
-            ->get();
+            ->whereIn('loai', ['van_ban', 'hinh_anh', 'chia_se', 'binh_chon']);
+
+        if (!empty($blockedUserIds)) {
+            $query->whereNotIn('nguoi_dung_id', $blockedUserIds);
+        }
+
+        if ($feedType === 'following' && $user) {
+            // Chronological feed: CHỈ lấy bài viết của những người mà User đang đăng nhập theo dõi
+            if (!empty($followingIds)) {
+                $query->whereIn('nguoi_dung_id', $followingIds);
+            } else {
+                // Nếu chưa theo dõi ai, trả về danh sách rỗng
+                $query->whereRaw('1 = 0');
+            }
+            $query->latest();
+        } else {
+            // Tab Dành cho bạn / Mặc định: Hiển thị tất cả bài viết trên toàn hệ thống, mới nhất lên đầu
+            $query->latest();
+        }
+
+        $posts = $query->paginate(10)->withQueryString();
+
+        if ($request->ajax()) {
+            return view('components.posts-feed', [
+                'posts'    => $posts,
+                'feedType' => $feedType,
+            ])->render();
+        }
 
         // Lấy stories chưa hết hạn, eager-load user
         $stories = Tin24h::with('user')
@@ -59,6 +93,7 @@ class HomeController extends Controller
             'posts'       => $posts,
             'stories'     => $stories,
             'recentMedia' => $recentMedia,
+            'feedType'    => $feedType,
         ]);
     }
 }
